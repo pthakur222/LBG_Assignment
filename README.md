a44fd5ec2f331c7e66b47c95096f58bd17d096de
Priyanka <priyanka.thakur5@wipro.com>
2018-09-17 16:21:17 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# LBG_Assignment
+
+#For ReadFile :
+run node ReadFile.js <filepath>
+
+Run the code with npm start and hit following urls:
+
+#ProductofParams
+http://localhost:3001/product?firstValue=<var1>&secondValue=<var2>
+
+#StringRead
+http://localhost:3001/string?string=<add your text>
+
+#WriteFile
+http://localhost:3001/writefile?rawData=<add your text>
+
+resultant file is output.txt
---
+++ server.js
@@ -0,0 +1,51 @@
+const http = require('http');
+const fs = require('fs');
+const express = require('express');
+const app = express();
+
+const func = require('./StringRead.js');
+const server = http.createServer(app);
+
+app.get('/product',function(req,res){
+    let firstValue = req.query.firstValue;
+    let secondValue = req.query.secondValue;
+    const product = firstValue * secondValue;
+    if(!product){
+        if(isNaN(firstValue) || isNaN(secondValue)){
+            res.send("Value is not a number. Please provide numerical value")
+        }
+        else{
+        res.sendStatus(404);
+        res.send("Not found");
+        }
+    }
+    else{
+    res.end("Product is:" + product);
+    }
+})
+
+app.get('/string',function(req,res){
+    var str = req.query.string;
+     const result = func.readString(str);
+     res.end(result);
+ })
+
+ 
+app.get('/writefile',function(req,res){
+
+    var rawData = req.query.rawData;
+    fs.writeFile('output.txt',rawData, (err) => {
+        if(err){
+          res.send(err)
+         }
+         else{
+             res.statusCode = 200;
+             res.end("File saved successfully as output.txt");
+         }
+       });
+})
+
+
+server.listen(3001,function(){
+    console.log("Server listening on port 3001");
+});
